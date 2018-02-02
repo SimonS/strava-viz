0c39ede0469247bd84d513fa99ed171edbd492e4
Simon Scarfe <simon.scarfe@gmail.com>
2018-02-02 22:51:22 +0000

    more tinkering

--- src/strava_viz/fetch.clj
+++ src/strava_viz/fetch.clj
@@ -7,24 +7,25 @@
 
 (def tkn (env :strava-access-token))
 
-(defn get-epochs-for-this-week
-  ([] (get-epochs-for-this-week (t/now)))
-  ([todays-date]
+(defn get-epochs-for-this-week [todays-date]
     (let [today (t/floor todays-date t/day)
           today-day (t/day-of-week today)
           monday-diff (- today-day 1)
           monday-date (t/minus- today (t/days monday-diff))
           next-monday-date (t/plus- monday-date (t/days 7))]
       [(tc/to-epoch monday-date)
-       (tc/to-epoch next-monday-date)])))
+       (tc/to-epoch next-monday-date)]))
 
 (defn get-runs [monday next-monday]
   (<!! (strava/activities tkn {"before" next-monday "after" monday})))
 
-(defn format-runs [runs]
-  (map #(assoc % :end_date_local (tc/to-string (t/plus-
-                                   (tc/from-string (:start_date_local %))
-                                   (t/seconds (Integer. (:elapsed_time %)))))) runs))
+(defn assoc-end-date [run]
+  (assoc run :end_date_local
+    (tc/to-string (t/plus-
+                    (tc/from-string (:start_date_local run))
+                    (t/seconds (Integer. (:elapsed_time run)))))))
+
+(defn format-runs [runs] (map assoc-end-date runs))
 
 (defn get-and-format-runs
   ([] (get-and-format-runs (t/now)))
